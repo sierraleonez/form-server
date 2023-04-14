9253f2300281700e974dceea2f534e78b2c35306
Almer Andhika <almerandhika@DXTR-IT-ALMER.local>
2023-04-14 14:09:40 +0700

    update page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Autocomplete,
   Backdrop,
   Button,
@@ -11,10 +14,13 @@ import {
   MenuItem,
   Select,
   TextField,
+  Typography,
 } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useEffect, useState } from "react";
 import { Fetcher } from "@/helper/axios";
 import { useFormik } from "formik";
+import { boolean, number, object, string } from "yup";
 
 type Student = {
   id: number;
@@ -35,7 +41,7 @@ type PostFormPayload = {
 export default function Home() {
   const [selectedName, setSelectedName] = useState<string>("");
   const [listStudent, setListStudent] = useState<Array<string>>([]);
-  const [loading, setLoading] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false);
   const [openSnack, setOpenSnack] = useState<boolean>(false);
 
   function postFormData(data: {
@@ -43,25 +49,25 @@ export default function Home() {
     phoneNumber: any;
     available: any;
   }) {
-    setTimeout(() => {
-    }, 2000)
+    if (!selectedName) {
+      return
+    }
     try {
-      setLoading(true)
+      setLoading(true);
       const payload: PostFormPayload = {
         email: data.email || "",
         phoneNumber: String(data.phoneNumber),
         isComing: Boolean(data.available),
         participantId: Number(selectedName.split("-")[0]),
       };
-      Fetcher.post("/form", payload).then(res => {
-        setSelectedName("")
-        formik.resetForm()
+      Fetcher.post("/form", payload).then((res) => {
+        setSelectedName("");
+        formik.resetForm();
         setOpenSnack(true);
-        setLoading(false)
-        // console.log(res)
-      })
+        setLoading(false);
+      });
     } catch (err) {
-      setLoading(false)
+      setLoading(false);
       console.log(err);
     }
   }
@@ -78,6 +84,11 @@ export default function Home() {
     getFormData();
   }, []);
 
+  const validationSchema = object({
+    email: string().email("Enter valid email"),
+    phoneNumber: number(),
+    available: boolean().required("Please fill your availability"),
+  });
   const formik = useFormik({
     initialValues: {
       available: 0,
@@ -85,10 +96,27 @@ export default function Home() {
       phoneNumber: "",
     },
     onSubmit: (res) => postFormData(res),
+    validationSchema: validationSchema,
   });
 
   return (
     <div className="bg-white flex-column flex-auto h-screen p-4 space-y-4">
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Event Detail</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            18 April 2023 <br/>
+            BigBerry Tegal <br/>
+            Urunan: 100k
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       <h1 className="text-black underline">DivertyOne Bukber Form</h1>
       <form onSubmit={formik.handleSubmit} className="space-y-4">
         <div className="space-y-4">
@@ -115,6 +143,9 @@ export default function Home() {
             <Select
               labelId="demo-simple-select-label"
               onChange={formik.handleChange}
+              error={
+                formik.touched.available && Boolean(formik.errors.available)
+              }
               id="available"
               name="available"
               label="Available?"
@@ -131,20 +162,36 @@ export default function Home() {
           <FormControl fullWidth className="space-y-4">
             <TextField
               label="Email"
+              helperText={formik.touched.email && formik.errors.email}
+              error={formik.touched.email && Boolean(formik.errors.email)}
               name="email"
+              type="email"
               value={formik.values.email}
               onChange={formik.handleChange}
             />
             <TextField
               label="Phone Number"
               name="phoneNumber"
+              type="number"
+              helperText={
+                formik.touched.phoneNumber && formik.errors.phoneNumber
+              }
+              error={
+                formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)
+              }
               value={formik.values.phoneNumber}
               onChange={formik.handleChange}
             />
           </FormControl>
         </div>
 
-        <Button color="primary" variant="contained" fullWidth type="submit" className="text-black">
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+          className="text-black"
+        >
           Submit
         </Button>
 
@@ -156,15 +203,11 @@ export default function Home() {
             <p>See you there!</p>
           </DialogContent>
         </Dialog>
-        {/* <Snackbar
-          open={openSnack}
-          autoHideDuration={1000}
-          onClose={() => setOpenSnack(false)}
-        >
-          <Alert severity="success">Your data successfully submitted</Alert>
-        </Snackbar> */}
         <Backdrop
-          sx={{ color: "#fff", zIndex: (theme: any) => theme.zIndex.drawer + 1 }}
+          sx={{
+            color: "#fff",
+            zIndex: (theme: any) => theme.zIndex.drawer + 1,
+          }}
           open={loading}
         >
           <CircularProgress color="inherit" />
